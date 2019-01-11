49dcad1bd3feeb96b264e9c1b3dc9441bfec7cbc
Laurens Jordense <laurens.jordense@gmail.com>
2019-01-11 12:03:46 +0100

    Basic Stoplicht
    Dit is een standaard software voor een stoplicht. Deze kan gebruikt worden voor elke stoplicht.
    
    Als het bijna rood wordt, gaat de groene lamp steeds sneller knipperen aan het einde.

---
+++ Basic_Stoplicht_Software/Basic_Stoplicht_Software.ino
@@ -0,0 +1,99 @@
+int rood1 = 13;
+int groen1 = 12;
+
+void setup() {
+
+  pinMode(rood1, OUTPUT);
+  pinMode(groen1, OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(groen1, HIGH);
+  digitalWrite(rood1, LOW);
+  delay(25000);
+  
+  digitalWrite(groen1, LOW);
+  delay(1000);
+  digitalWrite(groen1, HIGH);
+  delay(1000);
+  digitalWrite(groen1, LOW);
+  delay(500);
+  digitalWrite(groen1, HIGH);
+  delay(500);
+  digitalWrite(groen1, LOW);
+  delay(500);
+  digitalWrite(groen1, HIGH);
+  delay(500);
+  digitalWrite(groen1, LOW);
+  delay(250);
+  digitalWrite(groen1, HIGH);
+  delay(250);
+  digitalWrite(groen1, LOW);
+  delay(250);
+  digitalWrite(groen1, HIGH);
+  delay(250);
+  digitalWrite(groen1, LOW);
+  delay(250);
+  digitalWrite(groen1, HIGH);
+  delay(250);
+  digitalWrite(groen1, LOW);
+  delay(250);
+  digitalWrite(groen1, HIGH);
+  delay(250);
+  digitalWrite(groen1, LOW);
+  delay(100);
+  digitalWrite(groen1, HIGH);
+  delay(100);
+  digitalWrite(groen1, LOW);
+  delay(100);
+  digitalWrite(groen1, HIGH);
+  delay(100);
+  digitalWrite(groen1, LOW);
+  delay(100);
+  digitalWrite(groen1, HIGH);
+  delay(100);
+  digitalWrite(groen1, LOW);
+  delay(100);
+  digitalWrite(groen1, HIGH);
+  delay(100);
+  digitalWrite(groen1, LOW);
+  delay(50);
+  digitalWrite(groen1, HIGH);
+  delay(50);
+  digitalWrite(groen1, LOW);
+  delay(50);
+  digitalWrite(groen1, HIGH);
+  delay(50);
+  digitalWrite(groen1, LOW);
+  delay(50);
+  digitalWrite(groen1, HIGH);
+  delay(50);
+  digitalWrite(groen1, LOW);
+  delay(50);
+  digitalWrite(groen1, HIGH);
+  delay(50);
+   digitalWrite(groen1, LOW);
+  delay(50);
+  digitalWrite(groen1, HIGH);
+  delay(50);
+  digitalWrite(groen1, LOW);
+  delay(50);
+  digitalWrite(groen1, HIGH);
+  delay(50);
+  digitalWrite(groen1, LOW);
+  delay(50);
+  digitalWrite(groen1, HIGH);
+  delay(50);
+  digitalWrite(groen1, LOW);
+  delay(50);
+  digitalWrite(groen1, HIGH);
+  delay(50);
+  
+  
+  
+  digitalWrite(rood1, HIGH);
+  digitalWrite(groen1, LOW);
+  delay(30000);
+}
