74e0044907b208e1f7926679cd9814e3afeec49f
Laurens Jordense <laurens.jordense@gmail.com>
2019-01-31 13:46:15 +0100

    Een Rewrite van de code
    In deze code heb ik het hele programma opnieuw geschreven. Hierbij heb ik ook een eigen delay functie gemaakt.

---
+++ Stoplicht_Rewrite/Stoplicht_Rewrite.ino
@@ -0,0 +1,241 @@
+int rood1 = 13;
+int groen1 = 12;
+int rood2 = 11;
+int groen2 = 10;
+int rood3 = 9;
+int groen3 = 8;
+int rood4 = 7;
+int groen4 = 6;
+
+int vgRood = 5;
+int vgGroen = 4;
+int vgBtn = 3;
+int vgBtnState = 0;
+int vgAct = 0;
+
+int buslijnBtn = 2;
+int buslijnBtnState = 0;
+int buslijnAct = 0;
+
+int i = 0;
+
+void setup() {
+
+  // Stoplicht 1
+  pinMode(rood1, OUTPUT);
+  pinMode(groen1, OUTPUT);
+
+  // Stoplicht 2
+  pinMode(rood2, OUTPUT);
+  pinMode(groen2, OUTPUT);
+
+  // Stoplicht 3
+  pinMode(rood3, OUTPUT);
+  pinMode(groen3, OUTPUT);
+
+  // Stoplicht 4
+  pinMode(rood4, OUTPUT);
+  pinMode(groen4, OUTPUT);
+
+  // Buslijn
+  pinMode(buslijnBtn, INPUT);
+
+  // Voetgangers
+  pinMode(vgRood, OUTPUT);
+  pinMode(vgGroen, OUTPUT);
+  pinMode(vgBtn, INPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  buslijnBtnState = digitalRead(buslijnBtn);
+  vgBtnState = digitalRead(vgBtn);
+  if (buslijnBtnState == HIGH || buslijnAct == 1) {
+    sl2();
+  } else if (vgBtnState == HIGH || vgAct == 1) {
+    vgSl();
+  } else {
+    sl1();
+  }
+
+  buslijnBtnState = digitalRead(buslijnBtn);
+  vgBtnState = digitalRead(vgBtn);
+  if (buslijnBtnState == HIGH || buslijnAct == 1) {
+    sl2();
+  } else if (vgBtnState == HIGH || vgAct == 1) {
+    vgSl();
+  } else {
+    sl2();
+  }
+
+  buslijnBtnState = digitalRead(buslijnBtn);
+  vgBtnState = digitalRead(vgBtn);
+  if (buslijnBtnState == HIGH || buslijnAct == 1) {
+    sl2();
+  } else if (vgBtnState == HIGH || vgAct == 1) {
+    vgSl();
+  } else {
+    sl3();
+  }
+
+  buslijnBtnState = digitalRead(buslijnBtn);
+  vgBtnState = digitalRead(vgBtn);
+  if (buslijnBtnState == HIGH || buslijnAct == 1) {
+    sl2();
+  } else if (vgBtnState == HIGH || vgAct == 1) {
+    vgSl();
+  } else {
+    sl4();
+  }
+
+}
+
+void wait(int secs) {
+
+  if (i <= secs) {
+    buslijnBtnState = digitalRead(buslijnBtn);
+    if (buslijnBtnState == HIGH) {
+      if (buslijnAct == 0) {
+        buslijnAct = 1;
+        Serial.println("Buslijn is geactiveerd!");
+      } else if (buslijnAct == 1) {
+        buslijnAct = 1;
+        Serial.println("Buslijn is geactiveerd!");
+      }
+    }
+    vgBtnState = digitalRead(vgBtn);
+    if (vgBtnState == HIGH) {
+      if (vgAct == 0) {
+        vgAct = 1;
+        Serial.println("Voetgangers is geactiveerd!");
+      } else if (vgAct = 1) {
+        vgAct = 1;
+        Serial.println("Voetgangers is geactiveerd!");
+      }
+    }
+    
+    i++;
+    delay(1);
+  } else {
+    secs = 0;
+    i = 0;
+  }
+}
+
+void sl1(){
+
+  digitalWrite(rood1, LOW);
+  digitalWrite(groen1, HIGH);
+  wait(10000);
+  digitalWrite(groen1, LOW);
+  wait(500);
+  digitalWrite(groen1, HIGH);
+  wait(500);
+  digitalWrite(groen1, LOW);
+  wait(500);
+  digitalWrite(groen1, HIGH);
+  wait(500);
+  digitalWrite(groen1, LOW);
+  wait(500);
+  digitalWrite(groen1, HIGH);
+  wait(500);
+  digitalWrite(groen1, LOW);
+  wait(500);
+  digitalWrite(rood1, HIGH);
+  
+}
+
+void sl2(){
+
+  digitalWrite(rood2, LOW);
+  digitalWrite(groen2, HIGH);
+  wait(10000);
+  digitalWrite(groen2, LOW);
+  wait(500);
+  digitalWrite(groen2, HIGH);
+  wait(500);
+  digitalWrite(groen2, LOW);
+  wait(500);
+  digitalWrite(groen2, HIGH);
+  wait(500);
+  digitalWrite(groen2, LOW);
+  wait(500);
+  digitalWrite(groen2, HIGH);
+  wait(500);
+  digitalWrite(groen2, LOW);
+  wait(500);
+  digitalWrite(rood2, HIGH);
+  
+}
+
+void sl3(){
+
+  digitalWrite(rood3, LOW);
+  digitalWrite(groen3, HIGH);
+  wait(10000);
+  digitalWrite(groen3, LOW);
+  wait(500);
+  digitalWrite(groen3, HIGH);
+  wait(500);
+  digitalWrite(groen3, LOW);
+  wait(500);
+  digitalWrite(groen3, HIGH);
+  wait(500);
+  digitalWrite(groen3, LOW);
+  wait(500);
+  digitalWrite(groen3, HIGH);
+  wait(500);
+  digitalWrite(groen3, LOW);
+  wait(500);
+  digitalWrite(rood3, HIGH);
+  
+}
+
+void sl4(){
+
+  digitalWrite(rood4, LOW);
+  digitalWrite(groen4, HIGH);
+  wait(10000);
+  digitalWrite(groen4, LOW);
+  wait(500);
+  digitalWrite(groen4, HIGH);
+  wait(500);
+  digitalWrite(groen4, LOW);
+  wait(500);
+  digitalWrite(groen4, HIGH);
+  wait(500);
+  digitalWrite(groen4, LOW);
+  wait(500);
+  digitalWrite(groen4, HIGH);
+  wait(500);
+  digitalWrite(groen4, LOW);
+  wait(500);
+  digitalWrite(rood4, HIGH);
+  
+}
+
+void vgSl(){
+
+  digitalWrite(vgRood, LOW);
+  digitalWrite(vgGroen, HIGH);
+  wait(10000);
+  digitalWrite(vgGroen, LOW);
+  wait(500);
+  digitalWrite(vgGroen, HIGH);
+  wait(500);
+  digitalWrite(vgGroen, LOW);
+  wait(500);
+  digitalWrite(vgGroen, HIGH);
+  wait(500);
+  digitalWrite(vgGroen, LOW);
+  wait(500);
+  digitalWrite(vgGroen, HIGH);
+  wait(500);
+  digitalWrite(vgGroen, LOW);
+  wait(500);
+  digitalWrite(vgRood, HIGH);
+  
+}
